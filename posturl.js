f02ed192a4a1bfe3e7ec2372c37f677876529548
User13 <User13>
2017-05-17 19:32:20 +0300

    major logic

--- posturl.js
+++ posturl.js
@@ -9,9 +9,81 @@
         // only CommonJS-like environments that support module.exports,
         // like Node.
         module.exports = factory(true);
+    } else {
+    	var PostURL = factory();
+    	window.postURL = new PostURL();
     }
-}(this, function () {
+}(this, function(){
 
-   	
+   	var PostURL = function(){
+   		window.addEventListener('hashchange', function(){
+   			if (this.onMessage){
+   				this.onMessage(this.read());
+   			}
+   		}.bind(this));
+   	};
+
+   	PostURL.prototype = {
+   		splitter : '::',
+   		onMessage : null,
+   		write : function(action, data){
+   			var message = this.pack(action, data);
+
+   			if (!message){
+   				return false;
+   			} else {
+   				this.add2URL(message);
+   				return message;
+   			}
+   		},
+   		read : function(){
+   			var href = window.location.href;
+   			var message = this.unpack(href);
+   		},
+   		add2URL : function(data){
+   			window.open('#' + data, '_self');
+   		},
+   		unpack : function(data){
+   			if (data.indexOf('#') < 0){
+   				return false;
+   			} else {
+   				var raw = data.split('#')[1];
+   				
+   				if (raw.indexOf(this.splitter) < 0){
+   					return raw;
+   				} else {
+   					raw = raw.split(this.splitter);
+
+   					try {
+   						return {
+	   						action : raw[0],
+	   						data : JSON.parse(raw[1])
+	   					}
+   					} catch (err){
+   						console.warn(err);
+   					}
+
+   				}
+
+   			}
+   		},
+   		pack : function(action, data){
+   			try {
+   				var result = '';
+
+	   			if (typeof data == 'undefined'){
+	   				result = result + action;
+	   			} else {
+	   				result = result + 'action' + this.splitter + JSON.stringify(data);
+	   			}
+
+	   			return result;
+   			} catch (err){
+   				console.warn(err);
+   			}
+   		}
+   	};
+
+   	return PostURL;
 
 }));
